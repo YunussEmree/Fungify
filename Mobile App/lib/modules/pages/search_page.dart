9a25948e187d65c7af4a28da56fe6a037ec6b8fb
YunussEmree <senyigityunusemre@gmail.com>
2025-02-23 21:30:05 +0300

    Search page improvements

--- Mobile App/lib/modules/pages/search_page.dart
+++ Mobile App/lib/modules/pages/search_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:fungi_app/modules/widgets/gradient_container.dart';
+import 'package:get/get.dart';
+import 'package:fungi_app/modules/main_controller.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -12,6 +14,38 @@ class SearchPage extends StatefulWidget {
 class _SearchPageState extends State<SearchPage> {
   final TextEditingController _searchController = TextEditingController();
   String _searchQuery = '';
+
+  final List<String> mushroomNames = [
+    'Amanita muscaria',
+    'Chlorociboria aeruginosa',
+    'Cytidia salicina',
+    'Helvella atra',
+    'Mycena rosea',
+    'Tuber melanosporum',
+    'Agaricus bisporus',
+    'Bisporella citrina',
+    'Clathrus ruber',
+    'Geastrum saccatum',
+    'Hydnellum peckii',
+    'Pleurotus djamor',
+    'Alloclavaria purpurea',
+    'Boletus edulis',
+    'Cordyceps sinensis',
+    'Gliophorus psittacinus',
+    'Lactarius indigo',
+    'Russula virescens',
+    'Amanita jacksonii',
+    'Cantharellus cibarius',
+    'Cortinarius sanguineus',
+    'Gyromitra fastigiata',
+    'Morchella esculenta',
+    'Tremella fuciformis',
+  ];
+
+  List<String> get filteredMushrooms => mushroomNames
+      .where((name) =>
+          name.toLowerCase().contains(_searchQuery.toLowerCase()))
+      .toList();
 
   @override
   Widget build(BuildContext context) {
@@ -44,7 +78,9 @@ class _SearchPageState extends State<SearchPage> {
         children: [
           _buildSearchField(),
           const SizedBox(height: 20),
-          if (_searchQuery.isNotEmpty) _buildSearchResults(),
+          Expanded(
+            child: _buildSearchResults(),
+          ),
         ],
       ),
     );
@@ -72,18 +108,18 @@ class _SearchPageState extends State<SearchPage> {
         begin: Alignment.topLeft,
         end: Alignment.bottomRight,
         colors: [
-          const Color(0xFF8B6BFF).withAlpha(51), // 0.2 opacity
-          const Color(0xFFFF6BE6).withAlpha(51), // 0.2 opacity
+          const Color(0xFF8B6BFF).withAlpha(51),
+          const Color(0xFFFF6BE6).withAlpha(51),
         ],
       ),
       borderRadius: BorderRadius.circular(30),
       border: Border.all(
-        color: const Color(0xFF8B6BFF).withAlpha(77), // 0.3 opacity
+        color: const Color(0xFF8B6BFF).withAlpha(77),
         width: 1,
       ),
       boxShadow: [
         BoxShadow(
-          color: Colors.black.withAlpha(51), // 0.2 opacity
+          color: Colors.black.withAlpha(51),
           blurRadius: 10,
           spreadRadius: 1,
         ),
@@ -95,7 +131,7 @@ class _SearchPageState extends State<SearchPage> {
     return InputDecoration(
       hintText: 'Enter mushroom name...',
       hintStyle: GoogleFonts.poppins(
-        color: const Color.fromARGB(128, 255, 255, 255), // 0.5 opacity
+        color: const Color.fromARGB(128, 255, 255, 255),
       ),
       prefixIcon: const Icon(
         Icons.search,
@@ -110,17 +146,116 @@ class _SearchPageState extends State<SearchPage> {
   }
 
   Widget _buildSearchResults() {
-    return Expanded(
-      child: GradientContainer(
-        child: Center(
-          child: Text(
-            'Search results will be displayed here',
-            style: GoogleFonts.poppins(
-              color: const Color.fromARGB(179, 255, 255, 255), // 0.7 opacity
-              fontSize: 16,
-            ),
+    if (_searchQuery.isEmpty) {
+      return _buildAllMushrooms();
+    }
+    return _buildFilteredMushrooms();
+  }
+
+  Widget _buildAllMushrooms() {
+    return ListView.builder(
+      itemCount: mushroomNames.length,
+      itemBuilder: (context, index) {
+        return _buildMushroomItem(mushroomNames[index]);
+      },
+    );
+  }
+
+  Widget _buildFilteredMushrooms() {
+    final filtered = filteredMushrooms;
+    if (filtered.isEmpty) {
+      return Center(
+        child: Text(
+          'No mushrooms found',
+          style: GoogleFonts.poppins(
+            color: const Color.fromARGB(179, 255, 255, 255),
+            fontSize: 16,
           ),
         ),
+      );
+    }
+    return ListView.builder(
+      itemCount: filtered.length,
+      itemBuilder: (context, index) {
+        return _buildMushroomItem(filtered[index]);
+      },
+    );
+  }
+
+  Widget _buildMushroomItem(String mushroomName) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 4),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            const Color(0xFF8B6BFF).withAlpha(51),
+            const Color(0xFFFF6BE6).withAlpha(51),
+          ],
+        ),
+        borderRadius: BorderRadius.circular(15),
+        border: Border.all(
+          color: const Color(0xFF8B6BFF).withAlpha(77),
+          width: 1,
+        ),
+      ),
+      child: ListTile(
+        title: Text(
+          mushroomName,
+          style: GoogleFonts.poppins(
+            color: Colors.white,
+            fontSize: 16,
+          ),
+        ),
+        onTap: () => _onMushroomSelected(mushroomName),
+        trailing: const Icon(
+          Icons.arrow_forward_ios,
+          color: Color(0xFFFF6BE6),
+          size: 20,
+        ),
+      ),
+    );
+  }
+
+  void _onMushroomSelected(String mushroomName) {
+    // TODO: Backend hazır olduğunda burada API çağrısı yapılacak
+    debugPrint('Selected mushroom: $mushroomName');
+    Get.dialog(
+      AlertDialog(
+        backgroundColor: const Color(0xFF2A0E5F),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+          side: BorderSide(
+            color: const Color(0xFF8B6BFF).withAlpha(77),
+            width: 1,
+          ),
+        ),
+        title: Text(
+          'Selected Mushroom',
+          style: GoogleFonts.poppins(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        content: Text(
+          mushroomName,
+          style: GoogleFonts.poppins(
+            color: const Color.fromARGB(230, 255, 255, 255),
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Get.back(),
+            child: Text(
+              'OK',
+              style: GoogleFonts.poppins(
+                color: const Color(0xFFFF6BE6),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
